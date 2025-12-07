e1d24ac42ac88eed02bcb5028dd5cfb050b5c08b
이재훈 <jhl2211768@gmail.com>
2025-12-07 20:12:48 +0900

    2739

---
+++ src/bin/p2739.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let num: i32 = input.trim().parse().unwrap();
+
+    for i in 1..=9 {
+        println!("{} * {} = {}", num, i, num * i);
+    }
+}
