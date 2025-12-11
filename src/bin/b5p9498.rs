00d5a8282d2b4b00d8d4be5fb9f89a35f138c986
이재훈 <jhl2211768@gmail.com>
2025-12-11 18:09:42 +0900

    b5p9498

---
+++ src/bin/b5p9498.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let score: i8 = input.trim().parse().unwrap();
+
+    let grade = match score {
+        90..=100 => 'A',
+        80..=89 => 'B',
+        70..=79 => 'C',
+        60..=69 => 'D',
+        _ => 'F',
+    };
+    
+    println!("{grade}");
+}
