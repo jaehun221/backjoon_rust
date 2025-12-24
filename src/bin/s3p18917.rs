34d08a48039b8c5454a3b80d804fe34ef0a53efd
이재훈 <jhl2211768@gmail.com>
2025-12-24 09:06:56 +0900

    s3p18917

--- src/bin/s3p18917.rs
+++ src/bin/s3p18917.rs
@@ -1,21 +1,47 @@
-use std::io;
+use std::io::{self, Read};
 
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut seq = input.split_whitespace(); 
 
-}
+    let n: usize = seq.next().unwrap().parse().unwrap();
+    
+    let mut sum: i64 = 0;
+    let mut xor: i64 = 0;
 
-fn add(lst: Vec<i32>, n: i32) -> Vec<i32> {
-    lst
-}
+    let mut out = String::new();
 
-fn del() {
+    for _ in 0..n {
 
-}
+        let query: i32 = seq.next().unwrap().parse().unwrap();
 
-fn sum() {
+        match query {
+            1 => {
+                let x: i64 = seq.next().unwrap().parse().unwrap();
+                sum += x;
+                xor ^= x;
+            }, 
+            
+            2 => {
+                let x: i64 = seq.next().unwrap().parse().unwrap();
+                sum -= x;
+                xor ^= x;
+            },
 
-}
+            3 => {
+                out.push_str(&sum.to_string());
+                out.push('\n');
+            },
 
-fn xor() {
+            4 => {
+                out.push_str(&xor.to_string());
+                out.push('\n');
+            },
+            _ => {},
+        
+        };
+    };
 
+    print!("{out}")
 }
